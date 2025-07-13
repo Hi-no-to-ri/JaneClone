9ba6a6982838cb651bdddc9a54e6d224091228e5
Hi-no-to-ri <81909001+Hi-no-to-ri@users.noreply.github.com>
2025-07-14 04:32:10 +0900

    vcpkg用のninjaスクリプトを追加

---
+++ ninja_build_vcpkg.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+CMAKE=$(which cmake)
+VCPKG_TOOLCHAIN_FILE="C:/vcpkg/scripts/buildsystems/vcpkg.cmake"
+
+if [ ! -e build ]; then mkdir build; fi
+cd build
+$CMAKE --version
+$CMAKE -G "Ninja" -DCMAKE_TOOLCHAIN_FILE=$VCPKG_TOOLCHAIN_FILE ..
+ninja $@
