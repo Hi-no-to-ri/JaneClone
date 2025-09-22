e17e8a8a2d4dc98554f86eb3a4a04cf03fe56f4c
Hi-no-to-ri <81909001+Hi-no-to-ri@users.noreply.github.com>
2025-09-23 08:50:31 +0900

    案の定まずかったのでまたCopilot製の差し戻し。まだテスト中なので良いのだけれど。

--- curlpp/src/curlpp/Form.cpp
+++ curlpp/src/curlpp/Form.cpp
@@ -73,7 +73,7 @@ void
 curlpp::HttpPost::clear()
 {
   if(mFirst != NULL) {
-    ::curl_mime_free(mFirst);
+    ::curl_formfree(mFirst);
     mFirst = NULL;
     mLast = NULL;
   }
@@ -145,22 +145,22 @@ curlpp::FormParts::File::add(::curl_httppost ** first,
   // One instance = One curl_httppost, so we don't
   // need to duplicate the memory.
   if(mContentType.empty()) {
-    curl_mime_init(first, 
+    curl_formadd(first, 
 		 last, 
-		 curl_mime_name,
+		 CURLFORM_PTRNAME,
 		 mName.c_str(), 
-		 curl_mime_filedata,
+		 CURLFORM_FILE,
 		 mFilename.c_str(),
 		 CURLFORM_END );
   }
   else {
-    curl_mime_init(first, 
+    curl_formadd(first, 
 		 last, 
-		 curl_mime_name, 
+		 CURLFORM_PTRNAME, 
 		 mName.c_str(), 
-		 curl_mime_filedata,
+		 CURLFORM_FILE,
 		 mFilename.c_str(),
-		 curl_mime_type,
+		 CURLFORM_CONTENTTYPE,
 		 mContentType.c_str(),
 		 CURLFORM_END);
   }
@@ -210,22 +210,22 @@ curlpp::FormParts::Content::add(::curl_httppost ** first,
   // We uses options that will copy internally the string (c_str),
   // so we don't need to worry about the memory.
   if(mContentType.empty()) {
-    curl_mime_init(first, 
+    curl_formadd(first, 
 		 last, 
-		 curl_mime_name, 
+		 CURLFORM_PTRNAME, 
 		 mName.c_str(), 
-		 curl_mime_data,
+		 CURLFORM_PTRCONTENTS,
 		 mContent.c_str(),
 		 CURLFORM_END);
   }
   else {
-    curl_mime_init(first, 
+    curl_formadd(first, 
 		 last, 
-		 curl_mime_name, 
+		 CURLFORM_PTRNAME, 
 		 mName.c_str(), 
-		 curl_mime_data,
+		 CURLFORM_PTRCONTENTS,
 		 mContent.c_str(),
-		 curl_mime_type,
+		 CURLFORM_CONTENTTYPE,
 		 mContentType.c_str(),
 		 CURLFORM_END );
   }
