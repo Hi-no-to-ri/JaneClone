71596c41b334c0ff8bd842a102853dd645a0069e
Hi-no-to-ri <81909001+Hi-no-to-ri@users.noreply.github.com>
2026-04-12 07:39:19 +0900

    Revert "Revert "enums""
    This reverts commit 71b8398e598cb9e2ba8ca3abab75050819e4d9b3.

--- src/enums.hpp
+++ src/enums.hpp
@@ -1,26 +1,25 @@
-// enums.hpp - janeclones' enums
-/*
-    JaneClone - A bulletin board viewer for 5ch.io-like BBSs
-    Copyright (C) 2012-2021 Hiroyuki Nagata
-    Copyright (C) 2026- Hi-no-to-ri
+/**
+ * enums.hpp - janeclones' enums
+ *
+ * Copyright (C) 2013-2014 Hiroyuki Nagata <idiotpanzer@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-    Contributor:
-        Hiroyuki Nagata <idiotpanzer@gmail.com>
-        Hi-no-to-ri <https://github.com/Hi-no-to-ri>
-*/
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contributor:
+ *	Hiroyuki Nagata <idiotpanzer@gmail.com>
+ */
 
 #ifndef ENUMS_HPP_
 #define ENUMS_HPP_
